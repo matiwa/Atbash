cc0c5e045a6c11d92455cfa1ff177e24a73c9415
matiwa <mateuszivan1994@wp.pl>
2021-06-04 15:32:39 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Atbash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tBText = new System.Windows.Forms.TextBox();
+            this.tBMessage = new System.Windows.Forms.TextBox();
+            this.btnoperation = new System.Windows.Forms.Button();
+            this.gBSource = new System.Windows.Forms.GroupBox();
+            this.rBFile = new System.Windows.Forms.RadioButton();
+            this.rBText = new System.Windows.Forms.RadioButton();
+            this.gBSource.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tBText
+            // 
+            this.tBText.Location = new System.Drawing.Point(13, 13);
+            this.tBText.Multiline = true;
+            this.tBText.Name = "tBText";
+            this.tBText.Size = new System.Drawing.Size(326, 284);
+            this.tBText.TabIndex = 0;
+            // 
+            // tBMessage
+            // 
+            this.tBMessage.Enabled = false;
+            this.tBMessage.Location = new System.Drawing.Point(462, 13);
+            this.tBMessage.Multiline = true;
+            this.tBMessage.Name = "tBMessage";
+            this.tBMessage.Size = new System.Drawing.Size(326, 284);
+            this.tBMessage.TabIndex = 1;
+            // 
+            // btnoperation
+            // 
+            this.btnoperation.Location = new System.Drawing.Point(364, 90);
+            this.btnoperation.Name = "btnoperation";
+            this.btnoperation.Size = new System.Drawing.Size(75, 74);
+            this.btnoperation.TabIndex = 2;
+            this.btnoperation.Text = "Encrypt or Decrypt";
+            this.btnoperation.UseVisualStyleBackColor = true;
+            this.btnoperation.Click += new System.EventHandler(this.Btngo_Click);
+            // 
+            // gBSource
+            // 
+            this.gBSource.Controls.Add(this.rBFile);
+            this.gBSource.Controls.Add(this.rBText);
+            this.gBSource.Location = new System.Drawing.Point(364, 13);
+            this.gBSource.Name = "gBSource";
+            this.gBSource.Size = new System.Drawing.Size(75, 71);
+            this.gBSource.TabIndex = 3;
+            this.gBSource.TabStop = false;
+            this.gBSource.Text = "Source";
+            // 
+            // rBFile
+            // 
+            this.rBFile.AutoSize = true;
+            this.rBFile.Location = new System.Drawing.Point(7, 43);
+            this.rBFile.Name = "rBFile";
+            this.rBFile.Size = new System.Drawing.Size(41, 17);
+            this.rBFile.TabIndex = 1;
+            this.rBFile.Text = "File";
+            this.rBFile.UseVisualStyleBackColor = true;
+            this.rBFile.CheckedChanged += new System.EventHandler(this.Selectsource);
+            // 
+            // rBText
+            // 
+            this.rBText.AutoSize = true;
+            this.rBText.Checked = true;
+            this.rBText.Location = new System.Drawing.Point(7, 20);
+            this.rBText.Name = "rBText";
+            this.rBText.Size = new System.Drawing.Size(46, 17);
+            this.rBText.TabIndex = 0;
+            this.rBText.TabStop = true;
+            this.rBText.Text = "Text";
+            this.rBText.UseVisualStyleBackColor = true;
+            this.rBText.CheckedChanged += new System.EventHandler(this.Selectsource);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 314);
+            this.Controls.Add(this.gBSource);
+            this.Controls.Add(this.btnoperation);
+            this.Controls.Add(this.tBMessage);
+            this.Controls.Add(this.tBText);
+            this.Name = "Form1";
+            this.Text = "Atbash";
+            this.gBSource.ResumeLayout(false);
+            this.gBSource.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tBText;
+        private System.Windows.Forms.TextBox tBMessage;
+        private System.Windows.Forms.Button btnoperation;
+        private System.Windows.Forms.GroupBox gBSource;
+        private System.Windows.Forms.RadioButton rBFile;
+        private System.Windows.Forms.RadioButton rBText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Atbash
+{
+    public partial class Form1 : Form
+    {
+        FileStream file = new FileStream("message.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+        char[] chars = new char[]{' ','!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/','0',
+            '1','2','3','4','5','6','7','8','9',':',';','<','=','>','?','@','A','Ą','B','C','Ć','D','E',
+            'Ę','F','G','H','I','J','K','L','Ł','M','N','Ń','O','Ó','P','Q','R','S','Ś','T','U','V','W',
+            'X','Y','Z','Ź','Ż','[','/',']','^','_','`','a','ą','b','c','ć','d','e','ę','f','g','h','i',
+            'j','k','l','ł','m','n','ń','o','ó','p','q','r','s','ś','t','u','v','w','x','y','z','ź','ż',
+            '{','|','}','~'}; //ASCII code combined with Polish diacritized letters
+
+        string message = "";
+        char[] messagechar;
+        char[] codetable;
+        public Form1()
+        {
+            InitializeComponent();
+            //creating a code table
+            int k =-1;
+            codetable = new char[chars.Length];
+            for (int i = codetable.Length-1; i >= 0; i--)
+            {
+                k++;
+                codetable[k] = chars[i];
+            }
+        }
+
+        //select a message input method
+        private void Selectsource(object sender, EventArgs e)
+        {
+            if (rBText.Checked == true) tBText.Enabled = true;
+            else if (rBFile.Checked == true) tBText.Enabled = false;
+        }
+
+        private void Btngo_Click(object sender, EventArgs e)
+        {
+            //loading the message from the text field and saving it to a file
+            if (rBText.Checked == true)
+            {
+                message = tBText.Text;
+                StreamWriter sw = new StreamWriter(file);
+                sw.WriteLine(message);
+                sw.Close();
+            }
+            //loading a message from a file and saving it to the text field
+            else if (rBFile.Checked == true)
+            {
+                StreamReader sr = new StreamReader(file);
+                message = sr.ReadLine();
+                sr.Close();
+                tBText.Text = message;
+            }
+
+            //message encryption or decryption
+            messagechar = message.ToCharArray();
+            for(int i = 0; i < messagechar.Length; i++)
+            {
+                //encrypting or decrypting messages works the same
+                for (int j = 0; j < chars.Length;j++) 
+                    if (messagechar[i] == chars[j]) tBMessage.Text += codetable[j];
+            }
+            //protection against the next operation
+            tBText.Enabled = false;
+            btnoperation.Enabled = false;
+            gBSource.Enabled = false;
+        }
+    }
+}
